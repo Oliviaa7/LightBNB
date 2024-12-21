f41814fd87149721b4354f629b9b4533d92e473c
Oliviaa7 <olivialanderson1994@gmail.com>
2024-12-20 22:32:29 -0800

    updated functions

--- LightBNB_WebApp/db/database.js
+++ LightBNB_WebApp/db/database.js
@@ -17,14 +17,22 @@ const pool = new Pool({
  * @return {Promise<{}>} A promise to the user.
  */
 const getUserWithEmail = function (email) {
-  let resolvedUser = null;
-  for (const userId in users) {
-    const user = users[userId];
-    if (user && user.email.toLowerCase() === email.toLowerCase()) {
-      resolvedUser = user;
-    }
-  }
-  return Promise.resolve(resolvedUser);
+
+  return pool
+    .query(
+    `SELECT * FROM users
+    WHERE email = $1 LIMIT 1;`, [email])
+    .then((result) => {
+      if (result.rows.length === 0) {
+        return null;
+      }
+      return result.rows[0];
+    })
+    .catch((err) => {
+      console.error('Error querying database: ', err);
+      throw err;
+    })
+
 };
 
 /**
@@ -33,7 +41,21 @@ const getUserWithEmail = function (email) {
  * @return {Promise<{}>} A promise to the user.
  */
 const getUserWithId = function (id) {
-  return Promise.resolve(users[id]);
+
+  return pool
+    .query(
+    `SELECT * FROM users
+    WHERE id = $1 LIMIT 1;`, [id])
+    .then((result) => {
+      if (result.rows.length === 0) {
+        return null;
+      }
+      return result.rows[0];
+    })
+    .catch((err) => {
+      console.error('Error querying database: ', err);
+      throw err;
+    })
 };
 
 /**
@@ -42,10 +64,21 @@ const getUserWithId = function (id) {
  * @return {Promise<{}>} A promise to the user.
  */
 const addUser = function (user) {
-  const userId = Object.keys(users).length + 1;
-  user.id = userId;
-  users[userId] = user;
-  return Promise.resolve(user);
+
+  const queryParams = [user.name, user.email, user.password];
+
+  return pool
+  .query(
+  `INSERT INTO users (name, email, password) 
+  VALUES ($1, $2, $3)
+  RETURNING *;`, queryParams)
+  .then((result) => {
+    return result.rows[0];
+  })
+  .catch((err) => {
+    console.error('Error adding user:', err.message);
+    throw err;
+  })
 };
 
 /// Reservations
@@ -69,10 +102,9 @@ const getAllReservations = function (guest_id, limit = 10) {
  */
 const getAllProperties = function (options, limit = 10) {
 
-  pool
+  return pool
     .query(`SELECT * FROM properties LIMIT $1;`, [limit])
     .then((result) => {
-      console.log(result.rows);
       return result.rows;
     })
     .catch((err) => {
