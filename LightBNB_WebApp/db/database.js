707265f71a3e3785bfbc19c88659f5044cba6f94
Oliviaa7 <olivialanderson1994@gmail.com>
2024-12-20 22:46:37 -0800

    update allReservations function

--- LightBNB_WebApp/db/database.js
+++ LightBNB_WebApp/db/database.js
@@ -89,7 +89,24 @@ const addUser = function (user) {
  * @return {Promise<[{}]>} A promise to the reservations.
  */
 const getAllReservations = function (guest_id, limit = 10) {
-  return getAllProperties(null, 2);
+  return pool
+  .query(`
+  SELECT reservations.*, properties.title, properties.cost_per_night, avg(property_reviews.rating) as average_rating
+  FROM reservations
+  JOIN properties ON properties.id = reservations.property_id
+  JOIN property_reviews ON properties.id = property_reviews.property_id
+  WHERE reservations.guest_id = $1
+  GROUP BY reservations.id, properties.title, properties.cost_per_night
+  ORDER BY reservations.start_date
+  LIMIT $2;
+    `, [guest_id, limit])
+  .then((result) => {
+    return result.rows;
+  })
+  .catch((err) => {
+    console.error('Error fetching reservations: ', err.message);
+    throw err;
+  })
 };
 
 /// Properties
